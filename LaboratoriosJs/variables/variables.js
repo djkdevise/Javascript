6c567e5e8e636a70bb01d8456e64a240ac78a7fd
Jamith Garcia <djkdevise@gmail.com>
2024-06-15 13:31:07 -0500

    varibles JS

---
+++ LaboratoriosJs/variables/variables.js
@@ -0,0 +1,53 @@
+//Variables
+var pesona = "Jamith";
+var saludo ="Hola";
+console.log(saludo, pesona);
+
+/*
+Ejercicio: Declaración de variables
+En este ejercicio, practicará la declaración de variables.
+
+Tareas
+Declare una nueva variable llamada petDog y asígnele el nombre Rex.
+
+Declare una nueva variable llamada petCat y asígnele el nombre Pepper.
+
+Registe en la consola la variable petDog.
+
+Registe en la consola la variable petCat.
+
+Registre en la consola el texto "My pet dog's name is: " y la variable petDog .
+
+Registre en la consola el texto "My pet cat's name is: " y la variable petCat.
+
+Declare otra variable y asígnele el nombre catSound. Asígnele la cadena "purr".
+
+Declare otra variable y asígnele el nombre dogSound. Asígnele la cadena "woof".
+
+Registre en la consola la variable petDog, luego la cadena "says", luego la variable dogSound.
+
+Registre en la consola la variable petCat, luego la cadena "says", luego la variable catSound.
+
+Reasigne el valor almacenado en catSound a la cadena "meow".
+
+Registre en la consola la variable petCat, luego la cadena "now says", luego la variable catSound.
+
+Asegúresede generar todas las variables. Siéntase libre de jugar.
+*/
+
+var petDog = "Rex"; 
+var petCat = "Pepper";
+console.log(petDog);
+console.log(petCat);
+
+console.log("My pet dog's name is: ", petDog);
+console.log("My pet cat's name is: ", petCat);
+
+var catSound = "purr";
+var dogSound = "woof";
+
+console.log(petDog, "says", dogSound);
+console.log(petCat, "says", catSound);
+
+catSound = "meow";
+console.log(petCat,  "now says", catSound);
